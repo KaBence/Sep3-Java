6d99e02749366367582ebf7e6c122734679ad126
Michaela202 <veselovskamichaela222@gmail.com>
2023-10-26 15:33:13 +0200

    the database connection

--- src/main/java/Bullshit.java
+++ src/main/java/Bullshit.java
@@ -1,2 +1,14 @@
+import Utility.DataBase.String.StringData;
+import Utility.DataBase.String.StringDataImplementation;
+
 public class Bullshit {
+
+
+
+    public static void main(String[] args) {
+        StringData data= new StringDataImplementation();
+       // data.addSOmeValueToThisTest("THIS IS BULLSHIT");
+        System.out.println(data.getString());
+    }
+
 }
---
+++ src/main/java/Utility/DataBase/String/StringData.java
@@ -0,0 +1,8 @@
+package Utility.DataBase.String;
+
+import java.util.ArrayList;
+
+public interface StringData {
+    String addSOmeValueToThisTest(String text);
+    ArrayList getString();
+}
