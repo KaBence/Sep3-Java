85847eb6ad5406317e33e5253a68de404a6c366d
KBence <kabaly.bence@gmail.com>
2023-10-26 17:41:06 +0200

    proto and maven are happy with each other now

--- src/main/java/Bullshit.java
+++ src/main/java/Bullshit.java
@@ -2,9 +2,6 @@ import Utility.DataBase.String.StringData;
 import Utility.DataBase.String.StringDataImplementation;
 
 public class Bullshit {
-
-
-
     public static void main(String[] args) {
         StringData data= new StringDataImplementation();
        // data.addSOmeValueToThisTest("THIS IS BULLSHIT");
